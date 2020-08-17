0665957accd274cefbf7b60cdb0793e8628d6e05
Jeovanni <jeovannildg@gmail.com>
2020-08-16 21:40:56 -0600

    completed order file

--- src/Order.js
+++ src/Order.js
@@ -4,8 +4,18 @@ const Order = (props) => {
     const [order, setOrder] = useState(props.pizza);
 
     return(
-        <div>
-            
+        <div className="order">
+            {props.pizza.map(p => (
+                <div className="pizza" key={p.id}>
+                    <h1>Pizza has been ordered!</h1>
+                    <p>Here are the details</p>
+                    <p>{p.name}</p>
+                    <p>{p.size}</p>
+                    <p>{p.sauce}</p>
+                    <p>{p.toppings}</p>
+                    <p>{p.specialInstructions}</p>
+                </div>
+            ))}
         </div>
     )
 }
