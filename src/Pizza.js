67dd086932f89930a3dcbec1c18d2a75d21de96a
Jeovanni <jeovannildg@gmail.com>
2020-08-16 20:41:59 -0600

    added radio buttons for sauces

--- src/Pizza.js
+++ src/Pizza.js
@@ -6,8 +6,11 @@ export default function Pizza () {
     const [pizzaState, setPizzaState] = useState({
         name: "",
         size: "",
-        toppings: ""
+        toppings: "",
+        sauce: "Original"
     });
+
+    const [sauce, sauceSelected] = useState("");
 
     return (
         <div className="pizza-form">
@@ -26,6 +29,17 @@ export default function Pizza () {
                         <option>Large</option>
                         <option>Extra Large</option>
                     </select>
+                </label>
+
+                <label htmlFor="sauce">
+                    <input id="pizza-sauce-original" type="radio"name="original" checked={sauce === "original"} onClick={() => sauceSelected('original')} className="sauceCheck"/>
+                    Original Sauce
+                    <input id="pizza-sauce-garlic" type="radio"name="garlic" checked={sauce === "garlic"} onClick={() => sauceSelected('garlic')} className="sauceCheck"/>
+                    Garlic Ranch
+                    <input id="pizza-sauce-ranch" type="radio"name="bbq" checked={sauce === "bbq"} onClick={() => sauceSelected('bbq')} className="sauceCheck"/>
+                    BBQ Sauce
+                    <input id="pizza-sauce-bbq" type="radio"name="spinach" checked={sauce === "spinach"} onClick={() => sauceSelected('spinach')} className="sauceCheck"/>
+                    Spinach Alfredo
                 </label>
             </form>
         </div>
