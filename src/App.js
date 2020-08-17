4dd4cfae4298b0591fb1bf302c06299d111fe56c
Jeovanni <jeovannildg@gmail.com>
2020-08-16 20:18:13 -0600

    added routes and links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Home from "./Home";
+import Pizza from "./Pizza";
+import { Route, Link} from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Link to='/'>Home</Link>
+      <Link to='/Pizza'>Pizza</Link>
+
+      <Route exact path='/' component={Home}/>
+      <Route path='/Pizza'component={Pizza}/>
     </>
   );
 };
